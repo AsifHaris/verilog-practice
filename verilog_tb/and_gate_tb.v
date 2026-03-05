e5e0d3e4b6b9c8b06f87e4f94f05689875f97030
Asif Haris <asifharis007@gmail.com>
2026-03-05 16:21:03 +0530

    testbench for AND gate added

---
+++ verilog_tb/and_gate_tb.v
@@ -0,0 +1,16 @@
+module and_gate_tb;
+reg a,b;
+wire y;
+and_gate uut(.a(a),.b(b),.y(y));
+initial begin
+a=1'b0;b=1'b0;#10
+a=1'b0;b=1'b1;#10
+a=1'b1;b=1'b0;#10
+a=1'b1;b=1'b1;#10
+$finish;
+end
+  always @(*)
+    begin
+      $monitor("a=%b b=%b y=%b",a,b,y);
+    end
+endmodule
